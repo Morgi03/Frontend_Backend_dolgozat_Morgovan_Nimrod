5efdc1326b8aa7b03887d90d771f885e8065f945
Kerecsényi Márk <karatelovag@gmail.com>
2022-09-28 13:01:29 +0200

    Index.html kialakítása, feladat elkezdése

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
 import './style.css';
+import 'bootstrap/dist/js/bootstrap.bundle.js';
+import 'bootstrap/dist/css/bootstrap.css';
 
+
+function init(){
+    
+
+}
+
+document.addEventListener('DOMContentLoaded',init);
